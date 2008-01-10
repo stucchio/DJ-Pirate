f5ac27b41832a529450af54fe8f7bd64942b90bf
stucchio@cims.nyu.edu (Macbook) <none>
2008-01-09 21:00:56 -0500

    Initial import of AJAX version of Mr. DJ.
    darcs-hash:20080110020056-879e2-ea8c104e207a5fc93bfb5366b9688c0344a2ec78.gz

---
+++ mrdj.py
@@ -0,0 +1,68 @@
+import web
+import mpdclient
+
+urls = ( '/', 'index',
+         '/simpleajax/status', 'status',
+         '/simpleajax/command/(.*)', 'command',
+         )
+
+
+def get_mpd(host="192.168.1.5", port="6600"):
+    return mpdclient.MpdController(host=host,port=6600)
+
+def get_status_xml():
+    song = get_mpd().getCurrentSong()
+    mpd_status = get_mpd().status()
+    status_str = "<?xml version=\"1.0\" ?> \n" + "<root>"
+    if song:
+        status_str += "<currentsong>" + song.title + ", by " + song.artist + "</currentsong>"
+    else:
+        status_str += "<currentsong>" + "No song playing" + "</currentsong>"
+    status_str += "<state>"+ str(mpd_status.state) + "</state>"
+    status_str += "<volume>" + str(mpd_status.volume) + "</volume>" + "</root>"
+    return status_str
+
+class index:
+    def GET(self):
+        print "Hello, world!"
+
+class index:
+    def GET(self):
+        print "Hello, world!"
+
+class status:
+    def GET(self):
+        print get_status_xml()
+
+class command:
+    commands = [ "prev", "next", "shuffle", "stop", "clear"]
+    tougher_commands = [ "voldown", "volup", "pause", "play"]
+
+    def GET(self,command):
+        if command in self.commands: getattr(get_mpd(), command)()
+        if command in self.tougher_commands: getattr(self, command)()
+        print get_status_xml()
+
+    def play(self):
+        if get_mpd().status().state < 2:
+            get_mpd().play(get_mpd().getPlaylistPosition()[0])
+        else:
+            get_mpd().play()
+
+    def pause(self):
+        if get_mpd().status().state < 2:
+            get_mpd().play(get_mpd().getPlaylistPosition()[0])
+        else:
+            get_mpd().play()
+
+
+    def voldown(self):
+        get_mpd().volume( get_mpd().status().volume - 5)
+
+    def volup(self):
+        get_mpd().volume( get_mpd().status().volume + 5)
+
+
+if __name__ == "__main__":
+    web.webapi.internalerror = web.debugerror
+    web.run(urls, globals(),web.reloader)
