31a3aa5f7d5c0f744130c484443dabc9a1fb73df
stucchio@cims.nyu.edu (blackbox) <none>
2008-01-12 13:12:38 -0500

    Forgot to add main.js file.
    darcs-hash:20080112181238-7eccd-70404f69a57f475340e708bba49931ea348ec70a.gz

---
+++ static/main.js
@@ -0,0 +1,203 @@
+function updateStatus(nowplaying, playlist) {
+    var httpRequest;
+    nowplaying.getElementById("playingsong").innerHTML = "Loading...";
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function() { updateStatusCallback(httpRequest,true, nowplaying,playlist); };
+    httpRequest.open('GET', '../simpleajax/status', true);
+    httpRequest.send('');
+}
+
+function runCommand(cmd){
+    var httpRequest;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function() { updateStatusCallback(httpRequest,false, document, parent.playlist.document); };
+    httpRequest.open('GET', '../simpleajax/command/' + cmd, true);
+    httpRequest.send('');
+}
+
+function updateStatusCallback(httpRequest,repeat, nowplaying, playlist) {
+
+    if (httpRequest.readyState == 4) {
+        if (httpRequest.status == 200) {
+            xmlresponse = httpRequest.responseXML;
+            root = xmlresponse.getElementsByTagName('root').item(0);
+
+            //Display current song/volume
+            nowplaying.getElementById("playingsong").innerHTML = xmlresponse.getElementsByTagName('currentsong')[0].childNodes[0].nodeValue;
+            nowplaying.getElementById("volume").innerHTML = 'Volume: ' + xmlresponse.getElementsByTagName('volume')[0].childNodes[0].nodeValue;
+
+            // Change text on Play/Pause Button
+            if (xmlresponse.getElementsByTagName('state')[0].childNodes[0].nodeValue == '2'){
+                nowplaying.getElementById("playpausebutton").value = "Pause";
+            }else {
+                nowplaying.getElementById("playpausebutton").value = "Play ";
+            }
+
+            //Update playlist
+            stylePlaylist(xmlresponse.getElementsByTagName('playlistposition')[0].childNodes[0].nodeValue, -1, playlist);
+
+            if (repeat){ //Now try this again in 20,000 microseconds
+                setTimeout("updateStatus(document, parent.playlist.document);", 20000);
+            }
+
+        } else {
+            nowplaying.getElementById("playingsong").innerHTML = "Check your network connection";
+            if (repeat){
+                setTimeout("updateStatus();", 5000);
+            }
+        }
+    }
+}
+
+function updatePlaylist(nowplaying, playlist, repeat) {
+    var httpRequest;
+    //document.getElementById("playlist").innerHTML = "Loading...";
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function() { updatePlaylistCallback(httpRequest, repeat, nowplaying, playlist); };
+    httpRequest.open('GET', '../simpleajax/playlist', true);
+    httpRequest.send('');
+}
+
+function updatePlaylistCallback(httpRequest, repeat, nowplaying, playlist) {
+
+    if (httpRequest.readyState == 4) {
+        if (httpRequest.status == 200) {
+            playlist.getElementById("playlist").innerHTML = httpRequest.responseText;
+
+            if (repeat){
+                setTimeout("updatePlaylist(parent.nowplaying.document, document, true);", 30000);
+            }
+
+        } else {
+            playlist.getElementById("playlist").innerHTML = "Check your network connection";
+            if (repeat){
+                setTimeout("updatePlaylist(document, parent.nowplaying.document," + repeat + ");", 5000);
+            }
+        }
+    }
+}
+
+function playSong(songnum){
+    song = document.getElementById("song"+songnum);
+    playSongXmlRequest(songnum, parent.nowplaying.document, document);
+    stylePlaylist(-1,songnum,document);
+}
+
+function playSongXmlRequest(songnum, nowplaying, playlist) {
+    var httpRequest;
+    nowplaying.getElementById("playingsong").innerHTML = "Loading...";
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+        if (httpRequest.overrideMimeType) {
+            httpRequest.overrideMimeType('text/xml');
+        }
+    }
+    else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+
+    if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function() { updateStatusCallback(httpRequest,true, nowplaying,playlist); };
+    httpRequest.open('GET', '../simpleajax/playsong/' + songnum, true);
+    httpRequest.send('');
+}
+
+
+function stylePlaylist(songnum, selectedsong, doc) {
+    var even = false;
+
+    // obtain a reference to the desired list
+    var lst = doc.getElementById('playlist');
+    if (! lst) { return; }
+
+    // by definition, lists can have multiple elements, so we'll have to get all my children (i.e. <li>'s)
+    var lbodies = lst.getElementsByTagName("li");
+
+    for (var h=0; h < lbodies.length; h++) { //Now loop over list elements, set class appropriately
+        lbodies[h].className = even ? "even" : "odd";
+        even =  ! even;
+        if (h+1 == ( parseInt(songnum) )){
+            lbodies[h].className = 'currentsong';
+        }
+        if (h == ( parseInt(selectedsong) )){
+            lbodies[h].className = 'selectedsong';
+        }
+
+    }
+}
+
