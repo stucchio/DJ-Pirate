b74d23ea78bc4ab36e579df4c1760f41009f2b50
stucchio@cims.nyu.edu (blackbox) <none>
2008-01-13 11:27:07 -0500

    Added viewdir template, which lets us view directories. Can't add files yet.
    darcs-hash:20080113162707-7eccd-871e7b4d5ef7d9763a0b28260b5d100ab1e5ca0c.gz

--- mrdj.py
+++ mrdj.py
@@ -6,6 +6,7 @@ urls = ( '/', 'index',
          '/simpleajax/command/(.*)', 'command',
          '/simpleajax/playlist', 'playlist',
          '/simpleajax/playsong/([0-9]*)', 'playsong',
+         '/viewdir/(.*)', 'viewdir',
          )
 
 render = web.template.render('templates/')
@@ -31,6 +32,14 @@ def get_playist_xml():
     playlist = get_mpd().playlist()
     return render.playlist(enumerate(playlist), get_mpd().getPlaylistPosition()[0])
 
+def lsinfo(path):
+    songs,dirs = [], []
+    for s in get_mpd().ls_full([path,]):
+        if isinstance(s,mpdclient.Song):
+            songs += [s,]
+        if isinstance(s,mpdclient.Directory):
+            dirs += [s,]
+    return songs,dirs
 
 class index:
     def GET(self):
@@ -79,6 +88,11 @@ class command:
 
     def volup(self):
         get_mpd().volume( 5 * int((get_mpd().status().volume + 5)/5))
+
+class viewdir:
+    def GET(self,path):
+        songs,dirs = lsinfo(path)
+        print render.viewdir(songs,dirs)
 
 
 if __name__ == "__main__":
